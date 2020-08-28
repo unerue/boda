c97e055b74e37f4b4facd68a6da9e43cdf1a96b5
Kyungsu <unerue@me.com>
2020-08-28 12:11:56 +0900

    modified augmentations

--- detection/utils/transforms.py
+++ detection/utils/transforms.py
@@ -1,20 +1,11 @@
+import random
 from typing import Tuple, List, Dict
 import numpy as np
 from torch import Tensor
 
 
 class Compose:
-    """Composes several augmentations together.
-    Args:
-        transforms (List[Transform]): list of transforms to compose.
-    Example:
-        >>> augmentations.Compose([
-        >>>     transforms.CenterCrop(10),
-        >>>     transforms.ToTensor(),
-        >>> ])
-    """
-
-    def __init__(self, transforms):
+    def __init__(self, transforms: List[object]):
         self.transforms = transforms
 
     def __call__(self, image, boxes=None, labels=None):
@@ -23,11 +14,21 @@ class Compose:
         return image, boxes, labels
 
 
+class ToTensor:
+    def __call__(self, image, boxes=None, labels=None):
+        image = torch.from_numpy(image.transpose(2, 0, 1))
+        boxes = torch.from_numpy(boxes)
+        return image, boxes, labels
+
+
 class Resize:
-    def __init__(self, size):
+    def __init__(self, size: Tuple[int, int]):
+        if not isinstance(size, tuple):
+            raise ValueError
+
         self.size = size
 
-    def __call__(self, image, boxes, labels=None):
+    def __call__(self, image, boxes: List[List[float]], labels=None):
         w, h = image.size
 
         image = image.resize(self.size)
@@ -38,264 +39,82 @@ class Resize:
 
 
 class Normalize:
-    def __init__(self, mean, std):
+    """
+    Transforms a BRG image made of floats in the range [0, 255] to whatever
+    input the current backbone network needs.
+    """
+    def __init__(self, mean: List[float], std: List[float], normlize: bool = True):
         self.mean = np.array(mean, dtype=np.float32)
         self.std = np.array(std,  dtype=np.float32)
 
     def __call__(self, image, boxes, labels=None):
         image = np.asarray(image, dtype=np.float32)
-        
-
-        # if self.transform.normalize:
-        image = (image - self.mean) / self.std
-        # elif self.transform.subtract_means:
-        # image = (image - self.mean)
-        # elif self.transform.to_float:
+        image = (image - self.mean) / self.std    
         image = image / 255
-
 
         return image, boxes, labels 
 
 
-class ToTensor:
-    def __call__(self, image, boxes=None, labels=None):
-        image = torch.from_numpy(image.transpose(2, 0, 1))
-        boxes = torch.from_numpy(boxes)
-        return image, boxes, labels
-
-def get_transforms(is_train=True):
-    return Compose([
-        Resize((448,448)),
-        Normalize(MEANS, STD),
-        ToTensor(),
-    ])
-
 class RandomSaturation(object):
-    def __init__(self, lower=0.5, upper=1.5):
+    def __init__(self, lower=0.5, upper=1.5, p=0.5):
+        assert upper >= lower, 'contrast upper must be >= lower.'
+        assert lower >= 0, 'contrast lower must be non-negative.'
+
         self.lower = lower
         self.upper = upper
-        assert self.upper >= self.lower, "contrast upper must be >= lower."
-        assert self.lower >= 0, "contrast lower must be non-negative."
-
-    def __call__(self, image, masks=None, boxes=None, labels=None):
-        if random.randint(2):
+        self.p = p
+        
+    def __call__(self, image, boxes=None, labels=None):
+        if random.random() > self.p:
             image[:, :, 1] *= random.uniform(self.lower, self.upper)
 
-        return image, masks, boxes, labels
+        return image, boxes, labels
 
 
 class RandomHue(object):
-    def __init__(self, delta=18.0):
+    def __init__(self, delta=18.0, p=0.5):
         assert delta >= 0.0 and delta <= 360.0
         self.delta = delta
+        self.p = p
 
-    def __call__(self, image, masks=None, boxes=None, labels=None):
-        if random.randint(2):
+    def __call__(self, image, boxes=None, labels=None):
+        if random.random() > self.p:
             image[:, :, 0] += random.uniform(-self.delta, self.delta)
             image[:, :, 0][image[:, :, 0] > 360.0] -= 360.0
             image[:, :, 0][image[:, :, 0] < 0.0] += 360.0
-        return image, masks, boxes, labels
 
-
-
-class RandomLightingNoise(object):
-    def __init__(self):
-        self.perms = ((0, 1, 2), (0, 2, 1),
-                      (1, 0, 2), (1, 2, 0),
-                      (2, 0, 1), (2, 1, 0))
-
-    def __call__(self, image, masks=None, boxes=None, labels=None):
-        # Don't shuffle the channels please, why would you do this
-
-        # if random.randint(2):
-        #     swap = self.perms[random.randint(len(self.perms))]
-        #     shuffle = SwapChannels(swap)  # shuffle channels
-        #     image = shuffle(image)
-        return image, masks, boxes, labels
-
-
-class ConvertColor(object):
-    def __init__(self, current='BGR', transform='HSV'):
-        self.transform = transform
-        self.current = current
-
-    def __call__(self, image, masks=None, boxes=None, labels=None):
-        if self.current == 'BGR' and self.transform == 'HSV':
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        elif self.current == 'HSV' and self.transform == 'BGR':
-            image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
-        else:
-            raise NotImplementedError
-        return image, masks, boxes, labels
+        return image, boxes, labels
 
 
 class RandomContrast(object):
-    def __init__(self, lower=0.5, upper=1.5):
+    def __init__(self, lower=0.5, upper=1.5, p=0.5):
+        assert upper >= lower, 'contrast upper must be >= lower.'
+        assert lower >= 0, 'contrast lower must be non-negative.'
         self.lower = lower
         self.upper = upper
-        assert self.upper >= self.lower, "contrast upper must be >= lower."
-        assert self.lower >= 0, "contrast lower must be non-negative."
-
-    # expects float image
-    def __call__(self, image, masks=None, boxes=None, labels=None):
-        if random.randint(2):
+        self.p = p
+        
+    def __call__(self, image, boxes=None, labels=None):
+        if random.random() > self.p:
             alpha = random.uniform(self.lower, self.upper)
             image *= alpha
-        return image, masks, boxes, labels
+
+        return image, boxes, labels
 
 
+class RandomFlip:
+    def __init__(self, p=0.5):
+        self.p = p
 
-
-class RandomFlip(object):
-    def __call__(self, image, masks, boxes, labels):
+    def __call__(self, image, boxes, labels):
         height , _ , _ = image.shape
-        if random.randint(2):
+        if random.random() > self.p:
             image = image[::-1, :]
             masks = masks[:, ::-1, :]
             boxes = boxes.copy()
             boxes[:, 1::2] = height - boxes[:, 3::-2]
-        return image, masks, boxes, labels
 
-
-class RandomRot90(object):
-    def __call__(self, image, masks, boxes, labels):
-        old_height , old_width , _ = image.shape
-        k = random.randint(4)
-        image = np.rot90(image,k)
-        masks = np.array([np.rot90(mask,k) for mask in masks])
-        boxes = boxes.copy()
-        for _ in range(k):
-            boxes = np.array([[box[1], old_width - 1 - box[2], box[3], old_width - 1 - box[0]] for box in boxes])
-            old_width, old_height = old_height, old_width
-        return image, masks, boxes, labels
+        return image, boxes, labels
 
 
 
-
-class RandomSampleCrop(object):
-    """Crop
-    Arguments:
-        img (Image): the image being input during training
-        boxes (Tensor): the original bounding boxes in pt form
-        labels (Tensor): the class labels for each bbox
-        mode (float tuple): the min and max jaccard overlaps
-    Return:
-        (img, boxes, classes)
-            img (Image): the cropped image
-            boxes (Tensor): the adjusted bounding boxes in pt form
-            labels (Tensor): the class labels for each bbox
-    """
-    def __init__(self):
-        self.sample_options = (
-            # using entire original input image
-            None,
-            # sample a patch s.t. MIN jaccard w/ obj in .1,.3,.4,.7,.9
-            (0.1, None),
-            (0.3, None),
-            (0.7, None),
-            (0.9, None),
-            # randomly sample a patch
-            (None, None),
-        )
-
-    def __call__(self, image, masks, boxes=None, labels=None):
-        height, width, _ = image.shape
-        while True:
-            # randomly choose a mode
-            mode = random.choice(self.sample_options)
-            if mode is None:
-                return image, masks, boxes, labels
-
-            min_iou, max_iou = mode
-            if min_iou is None:
-                min_iou = float('-inf')
-            if max_iou is None:
-                max_iou = float('inf')
-
-            # max trails (50)
-            for _ in range(50):
-                current_image = image
-
-                w = random.uniform(0.3 * width, width)
-                h = random.uniform(0.3 * height, height)
-
-                # aspect ratio constraint b/t .5 & 2
-                if h / w < 0.5 or h / w > 2:
-                    continue
-
-                left = random.uniform(width - w)
-                top = random.uniform(height - h)
-
-                # convert to integer rect x1,y1,x2,y2
-                rect = np.array([int(left), int(top), int(left+w), int(top+h)])
-
-                # calculate IoU (jaccard overlap) b/t the cropped and gt boxes
-                overlap = jaccard_numpy(boxes, rect)
-
-                # This piece of code is bugged and does nothing:
-                # https://github.com/amdegroot/ssd.pytorch/issues/68
-                #
-                # However, when I fixed it with overlap.max() < min_iou,
-                # it cut the mAP in half (after 8k iterations). So it stays.
-                #
-                # is min and max overlap constraint satisfied? if not try again
-                if overlap.min() < min_iou and max_iou < overlap.max():
-                    continue
-
-                # cut the crop from the image
-                current_image = current_image[rect[1]:rect[3], rect[0]:rect[2],
-                                              :]
-
-                # keep overlap with gt box IF center in sampled patch
-                centers = (boxes[:, :2] + boxes[:, 2:]) / 2.0
-
-                # mask in all gt boxes that above and to the left of centers
-                m1 = (rect[0] < centers[:, 0]) * (rect[1] < centers[:, 1])
-
-                # mask in all gt boxes that under and to the right of centers
-                m2 = (rect[2] > centers[:, 0]) * (rect[3] > centers[:, 1])
-
-                # mask in that both m1 and m2 are true
-                mask = m1 * m2
-
-                # [0 ... 0 for num_gt and then 1 ... 1 for num_crowds]
-                num_crowds = labels['num_crowds']
-                crowd_mask = np.zeros(mask.shape, dtype=np.int32)
-
-                if num_crowds > 0:
-                    crowd_mask[-num_crowds:] = 1
-
-                # have any valid boxes? try again if not
-                # Also make sure you have at least one regular gt
-                if not mask.any() or np.sum(1-crowd_mask[mask]) == 0:
-                    continue
-
-                # take only the matching gt masks
-                current_masks = masks[mask, :, :].copy()
-
-                # take only matching gt boxes
-                current_boxes = boxes[mask, :].copy()
-
-                # take only matching gt labels
-                labels['labels'] = labels['labels'][mask]
-                current_labels = labels
-
-                # We now might have fewer crowd annotations
-                if num_crowds > 0:
-                    labels['num_crowds'] = np.sum(crowd_mask[mask])
-
-                # should we use the box left and top corner or the crop's
-                current_boxes[:, :2] = np.maximum(current_boxes[:, :2],
-                                                  rect[:2])
-                # adjust to crop (by substracting crop's left,top)
-                current_boxes[:, :2] -= rect[:2]
-
-                current_boxes[:, 2:] = np.minimum(current_boxes[:, 2:],
-                                                  rect[2:])
-                # adjust to crop (by substracting crop's left,top)
-                current_boxes[:, 2:] -= rect[:2]
-
-                # crop the current masks to the same dimensions as the image
-                current_masks = current_masks[:, rect[1]:rect[3], rect[0]:rect[2]]
-
-                return current_image, current_masks, current_boxes, current_labels
