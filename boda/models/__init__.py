575d5e0f37c015d7a2b2178e1e6568e7396e1ec1
leeyeoreum02 <leeyeoreum01@gmail.com>
2022-01-13 19:13:49 +0900

    1/13: add cascade-mask-rcnn

--- boda/models/__init__.py
+++ boda/models/__init__.py
@@ -7,6 +7,7 @@
 # from .mask_rcnn import *
 # from .keypoint_rcnn import *
 from .efficientdet import *
+from .cascade_mask_rcnn import *
 # from .backbone_mobilenetv2 import *
 # from .backbone_mobilenetv3 import *
 from .backbone_efficientnet import *
---
+++ boda/models/cascade_mask_rcnn/__init__.py
@@ -0,0 +1,7 @@
+from .configuration_cascade_mask_rcnn import CascadeMaskRCNNConfig
+from .architecture_cascade_mask_rcnn import CascadeMaskRCNNModel
+
+
+__all__ = [
+    'CascadeMaskRCNNConfig', 'CascadeMaskRCNNModel',
+]
